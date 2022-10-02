ee0b997c1e4e6261e219921bf4a50ed1b76cc217
ElenaBarker <113217019+ElenaBarker@users.noreply.github.com>
2022-10-02 23:23:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -89,3 +89,14 @@ document.getElementById('next').addEventListener('click', (event) => {
 
 });
 
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
